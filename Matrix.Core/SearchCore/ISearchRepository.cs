7c41190f19ecc24696dadff79aa9133bdec086d1
amitstefen <amit.stefen@gmail.com>
2014-03-14 16:09:59 +0530

    Putting up an Interface for generic search purposes.
    Next is to add certain concrete implementation to explore ElasticSearch
    engine.

---
+++ Matrix.Core/SearchCore/ISearchRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix.Core.SearchCore
+{
+    public interface ISearchRepository
+    {
+        string Index<T>(T document, string index = "") where T : MXSearchDocument;
+
+        int IndexMany<T>(IList<T> documents, string index = "") where T : MXSearchDocument;
+
+        T GetOne<T>(string id, string index = "") where T : MXSearchDocument;
+    }
+}
